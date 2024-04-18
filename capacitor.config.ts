dc0895f89e7a98973e3cfb828bed1b8a7886654f
PaablooDR <pab25702@gmail.com>
2024-04-18 13:21:57 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ch33f-client',
+  webDir: 'www'
+};
+
+export default config;
